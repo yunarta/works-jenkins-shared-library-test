d1a8d6182644b0d0e2c130d71328fc4b0f22f9d4
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-18 00:11:38 +0800

    aa

--- vars/testUseClass.groovy
+++ vars/testUseClass.groovy
@@ -2,14 +2,10 @@ import com.mobilesolutionworks.test.TestClass
 import org.gradle.util.VersionNumber
 
 def call() {
-    try {
-        VersionNumber a = VersionNumber.parse("1.0.0-pa-0")
-        VersionNumber b = VersionNumber.parse("1.0.0-pa-2")
-        def compare = a.compareTo(b)
-        println("compare = ${compare}")
-    } catch (exception) {
-        exception.printStackTrace()
-    }
+    VersionNumber a = VersionNumber.parse("1.0.0-pa-0")
+    VersionNumber b = VersionNumber.parse("1.0.0-pa-2")
+    def compare = a.compareTo(b)
+    println("compare = ${compare}")
 
     def object = new TestClass()
     println("test = ${object}")
