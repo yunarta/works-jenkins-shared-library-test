d4f18e0b2df10eabef4bbb9ddc8e129664ce3c4c
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-17 23:44:19 +0800

    sda

--- src/org/gradle/util/VersionNumber.groovy
+++ src/org/gradle/util/VersionNumber.groovy
@@ -1,10 +1,14 @@
 package org.gradle.util
 
-class VersionNumber {
-    
+class VersionNumber implements Comparable<VersionNumber>, Serializable {
     static def parse(String versionString) {
         return "ABC"
 //        return VersionNumber(1, 2, 3, "alpha-1")
 //        return DEFAULT_SCHEME.parse(versionString)
+    }
+
+    @Override
+    int compareTo(VersionNumber o) {
+        return 0
     }
 }
