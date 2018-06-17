0115d590967e6231c53b014749176492c12156aa
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-18 00:04:24 +0800

    321

--- vars/testUseClass.groovy
+++ vars/testUseClass.groovy
@@ -6,13 +6,9 @@ def call() {
     println(object)
 
     try {
-        def version = VersionNumber.parse("1.0.0-pa-0")
-        println("version = " + version)
-        println("""
-        |${version.major}
-        |${version.minor}
-        |${version.patch}
-        |${version.qualifier}""".stripMargin())
+        def a = VersionNumber.parse("1.0.0-pa-0")
+        def b = VersionNumber.parse("1.0.0-pa-2")
+        a.compareTo(b)
     } catch (exception) {
         exception.printStackTrace()
     }
