86592c1757b072179c343e7b8f98cf5ffc14217b
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-17 23:07:48 +0800

    4

--- vars/testUseClass.groovy
+++ vars/testUseClass.groovy
@@ -1,6 +1,14 @@
 import com.mobilesolutionworks.test.TestClass
+import org.gradle.util.VersionNumber
 
 def call() {
     def object = new TestClass()
     println(object)
+
+    def version = VersionNumber.parse("1.0.0-pa-0")
+    println("""
+    |${version.major}
+    |${version.minor}
+    |${version.patch}
+    |${version.qualifier}""".stripMargin())
 }
