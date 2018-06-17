abfebf3a3ede9c75da876fef46ffa0f19f86582a
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-18 00:10:22 +0800

    aa

--- vars/testUseClass.groovy
+++ vars/testUseClass.groovy
@@ -2,9 +2,6 @@ import com.mobilesolutionworks.test.TestClass
 import org.gradle.util.VersionNumber
 
 def call() {
-    def object = new TestClass()
-    println(object)
-
     try {
         VersionNumber a = VersionNumber.parse("1.0.0-pa-0")
         VersionNumber b = VersionNumber.parse("1.0.0-pa-2")
@@ -13,4 +10,7 @@ def call() {
     } catch (exception) {
         exception.printStackTrace()
     }
+
+    def object = new TestClass()
+    println("test = ${object}")
 }
